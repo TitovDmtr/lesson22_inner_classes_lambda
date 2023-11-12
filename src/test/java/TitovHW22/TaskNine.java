5731e2aa7eba9f94cc4be89f79b4720ff2bc03ba
dmytro <dmytro.titov@airslate.com>
2023-11-11 20:28:40 -0800

    classwork-and-hw22+fix-warnings

--- src/test/java/TitovHW22/TaskNine.java
+++ src/test/java/TitovHW22/TaskNine.java
@@ -1,27 +1,12 @@
 package TitovHW22;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-
-import static java.util.Comparator.comparing;
 
 public class TaskNine {
     public static void main(String[] args) {
 
 //   Завдання 9. Створити список рядків. Ввести з клавіатури якусь кількість рядків. Використовуючи цикл знайти найдовший рядок.
 
-        // Find max size string with 'stream().max'
-//        ArrayList<String> arrayList = new ArrayList<>();
-//        arrayList.add("12345");
-//        arrayList.add("1234567890");
-//        arrayList.add("1234567890");
-//        arrayList.add("Ut convallis justo porttitor sem vulputate, quis luctus erat cursus");
-//
-//        String max = arrayList.stream().max(comparing(String::length)).get();
-//        System.out.println("The largest string is: " + max);
-
-        // Find max size string with 'for' cycle
         ArrayList<String> arrayList = new ArrayList<String>();
         arrayList.add("12345");
         arrayList.add("1234567890");
